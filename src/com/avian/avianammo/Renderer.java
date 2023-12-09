0fb34502cdb9da0bbae2e273d955fc2a686a4f9e
TaziSydney123 <tazisydney@gmail.com>
2023-12-09 08:44:25 -0500

    Removed unused imports

--- src/com/avian/avianammo/Renderer.java
+++ src/com/avian/avianammo/Renderer.java
@@ -1,8 +1,5 @@
 package avianammo;
 
-import java.awt.Graphics2D;
-import java.awt.GraphicsConfiguration;
-import java.awt.GraphicsEnvironment;
 import java.awt.image.BufferedImage;
 
 public abstract class Renderer {
