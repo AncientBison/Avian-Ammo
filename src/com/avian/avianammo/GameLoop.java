e0183c9e48415031000bfa97f59d311b6ec6d89f
TaziSydney123 <tazisydney@gmail.com>
2023-12-09 21:49:19 -0500

    Added function to heat zone

--- src/com/avian/avianammo/GameLoop.java
+++ src/com/avian/avianammo/GameLoop.java
@@ -11,9 +11,9 @@ public class GameLoop extends TimerTask  {
     public static final int TICKS_PER_SECOND = 60;
     private final Controls controls;
     
-    private Seagull seagull;
-    private Seagull opponentSeagull;
-    private GameSocket socket;
+    private final Seagull seagull;
+    private final Seagull opponentSeagull;
+    private final GameSocket socket;
 
     public GameLoop(Seagull seagull, Seagull opponentSeagull, Controls controls, GameSocket socket) {
         this.seagull = seagull;
@@ -26,8 +26,7 @@ public class GameLoop extends TimerTask  {
     public void run() {
         seagull.tick(1.0 / GameLoop.TICKS_PER_SECOND);
 
-        if (socket.getLatestInformationData() != null && opponentSeagull.getMovement() instanceof RemoteMovement) {
-            RemoteMovement movement = (RemoteMovement)opponentSeagull.getMovement();
+        if (socket.getLatestInformationData() != null && opponentSeagull.getMovement() instanceof RemoteMovement movement) {
 
             double x = socket.getLatestInformationData().opponentX();
             double y = socket.getLatestInformationData().opponentY();
@@ -73,23 +72,24 @@ public class GameLoop extends TimerTask  {
             movement.setDirection(animationDirection);
         }
 
-        if (seagull.getMovement() instanceof PhysicsMovement) {
-            PhysicsMovement movement = (PhysicsMovement)seagull.getMovement();
+        if (seagull.getMovement() instanceof PhysicsMovement movement) {
 
-            double forceToAdd = controls.getLeft().isKeyPressed() ? -PhysicsConstants.SEAGULL_SPEED : 0;
-            forceToAdd += controls.getRight().isKeyPressed() ? PhysicsConstants.SEAGULL_SPEED : 0;
-            movement.addHorizontalForce(forceToAdd);
+            if (seagull.canMove()) {
+                double forceToAdd = controls.getLeft().isKeyPressed() ? -PhysicsConstants.SEAGULL_SPEED : 0;
+                forceToAdd += controls.getRight().isKeyPressed() ? PhysicsConstants.SEAGULL_SPEED : 0;
+                movement.addHorizontalForce(forceToAdd);
 
-            if (controls.getUp().isKeyDown()) {
-                movement.addVerticalForce(-PhysicsConstants.SEAGULL_JUMP_FORCE);
-                seagull.flapAnimation();
-            }
+                if (controls.getUp().isKeyDown()) {
+                    movement.addVerticalForce(-PhysicsConstants.SEAGULL_JUMP_FORCE);
+                    seagull.flapAnimation();
+                }
 
-            if (controls.getPoop().isKeyDown() && seagull.canPoop()) {
-                try {
-                    seagull.createPoop();
-                } catch (IOException e) {
-                    e.printStackTrace();
+                if (controls.getPoop().isKeyDown() && seagull.canPoop()) {
+                    try {
+                        seagull.createPoop();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
 
